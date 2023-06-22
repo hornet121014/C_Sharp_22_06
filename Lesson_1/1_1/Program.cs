219d12209e0b48a9472fd39a03c30a9096610085
AndreiE <andre.nikolasius@gmail.com>
2023-06-22 21:34:36 +0300

    add 1_1 in Lesson_1

---
+++ Lesson_1/1_1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Write a number 1: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write a number 2: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 == num2 * num2)
+{
+    Console.WriteLine("+");
+}
+else
+{
+    Console.WriteLine("-");
+}
