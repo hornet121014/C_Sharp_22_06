211c695e0ff4f0d40b0917baabe4d2ecd6f55adf
AndreiE <andre.nikolasius@gmail.com>
2023-06-22 22:50:10 +0300

    add HomeWork_1_2 in Lesson_1

---
+++ Lesson_1/HomeWork_1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Write a number 1: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Write a number 2: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine("max = "+num1+", min = "+num2);
+}
+else if(num1<num2)
+{
+    Console.WriteLine("max = "+num2+", min = "+num1);
+}
+else if(num1==num2)
+{
+    Console.WriteLine("number 1 = number 2 = "+num1);
+}
