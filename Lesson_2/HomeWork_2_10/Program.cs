984be2a925c56c7b6567e71bc18df2aa1ac16ad4
AndreiE <andre.nikolasius@gmail.com>
2023-06-27 00:56:39 +0300

    fix number < 0 in lesson 2 Homework 2_10

--- Lesson_2/HomeWork_2_10/Program.cs
+++ Lesson_2/HomeWork_2_10/Program.cs
@@ -5,8 +5,11 @@
 }
 Console.Write("Введите трехзначное число: ");
 int Num = int.Parse(Console.ReadLine()!);
+
 if (Num > 99 && Num < 1000)
     Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(Num)}");
+else if (Num < -99 && Num > -1000)
+    Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(Num) * -1}");
 else
     Console.WriteLine(
         "Вы ввели не число, которое не является трехзначным. Повторите попытку заново..."
