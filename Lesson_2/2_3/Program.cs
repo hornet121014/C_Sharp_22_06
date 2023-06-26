b1b43a6d321402288b14757bbe8fbb2005196de2
AndreiE <andre.nikolasius@gmail.com>
2023-06-26 21:33:29 +0300

    add lesson 2 work 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,15 @@
+﻿void chat(int a)
+{
+    if (a % 7 == 0 && a % 23 == 0)
+    {
+        Console.WriteLine("да");
+    }
+    else 
+    {  
+         Console.WriteLine("нет");
+    }  
+}
+Console.Write("Введите число: ");
+int NumA = int.Parse(Console.ReadLine()!);
+
+chat(NumA);
