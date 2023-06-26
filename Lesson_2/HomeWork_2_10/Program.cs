29cf479c6e421107cfcd19253f7dd3d49b51a02c
AndreiE <andre.nikolasius@gmail.com>
2023-06-26 23:16:47 +0300

    add lesson 2 Homework 2_10

---
+++ Lesson_2/HomeWork_2_10/Program.cs
@@ -0,0 +1,13 @@
+﻿int two_number(int num)
+{
+    num = num / 10;
+    return num % 10;
+}
+Console.Write("Введите трехзначное число: ");
+int Num = int.Parse(Console.ReadLine()!);
+if (Num > 99 && Num < 1000)
+    Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(Num)}");
+else
+    Console.WriteLine(
+        "Вы ввели не число, которое не является трехзначным. Повторите попытку заново..."
+    );
