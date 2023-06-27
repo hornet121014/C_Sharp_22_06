68bdede04defa7e32738da279d6f154def68121c
AndreiE <andre.nikolasius@gmail.com>
2023-06-27 20:56:48 +0300

    add task condition in comment HomeWork_2_10 and add new function abs()

--- Lesson_2/HomeWork_2_10/Program.cs
+++ Lesson_2/HomeWork_2_10/Program.cs
@@ -1,16 +1,27 @@
-﻿int two_number(int num)
+﻿/* 
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 
+*/
+
+int abs(int num)
+{
+    if (num<0) return num*-1;
+    return num;
+}
+
+int two_number(int num)
 {
     num = num / 10;
     return num % 10;
 }
+
 Console.Write("Введите трехзначное число: ");
 int Num = int.Parse(Console.ReadLine()!);
 
-if (Num > 99 && Num < 1000)
-    Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(Num)}");
-else if (Num < -99 && Num > -1000)
-    Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(Num) * -1}");
+if (abs(Num) > 99 && abs(Num) < 1000)
+    Console.WriteLine($"Вторая цифра, введенного трехзначного числа, равна {two_number(abs(Num))}");
 else
     Console.WriteLine(
-        "Вы ввели не число, которое не является трехзначным. Повторите попытку заново..."
-    );
+        "Вы ввели не число, которое не является трехзначным. Повторите попытку заново...");
