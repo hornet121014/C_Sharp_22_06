a91dcdb7aa4fada8c555fe010af087e752c985b8
AndreiE <andre.nikolasius@gmail.com>
2023-06-27 22:12:43 +0300

    fix function one_third_number() HomeWork_2_xx

--- Lesson_2/HomeWork_2_xx/Program.cs
+++ Lesson_2/HomeWork_2_xx/Program.cs
@@ -4,14 +4,12 @@
 918 -> 98
 */
 
-int one_third_number(int num)
+void one_third_number(int num)
 {
-    int num3=num%10;
-    int num1=num/100;
-    num = num1*10+num3;
-    return num;
+    num = (num/100)*10+num%10;
+    Console.WriteLine($"Новое число, полученное из трехзначного путем удаления второй цифры, равно {num}");
 }
 
 int Num=new Random().Next(100,1000);
 Console.WriteLine($"Случайное трехзначное число равно {Num}");
-Console.WriteLine($"Новое число, полученное из трехзначного путем удаления второй цифры, равно {one_third_number(Num)}");
+one_third_number(Num);
