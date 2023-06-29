9d615ac96777fd04ae59f1df4e316ea0cea9c11a
AndreiE <andre.nikolasius@gmail.com>
2023-06-29 22:05:25 +0300

    add Lesson 3 ClassWork 3_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N) 
+//    и выдаёт таблицу квадратов чисел от 1 до N.
+
+void step(int num)
+{
+    int i=1;
+    if (num<0) num=-num;
+    while (i<=num)
+    {
+        int result=i*i;
+        Console.Write(result+" ");
+        i++;   
+    }  
+}
+
+Console.Write("Input number: ");
+int num = int.Parse(Console.ReadLine()!);
+step(num);
