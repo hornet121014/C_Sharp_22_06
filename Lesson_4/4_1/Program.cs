f0f52a727183598a50cc83cbf503ab2feaae4cd7
AndreiE <andre.nikolasius@gmail.com>
2023-07-03 21:12:04 +0300

    add Lesson 4-1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// 1. Напишите программу, которая принимает
+//    на вход число и выдаёт количество цифр в числе.
+
+int kolC(int num)
+{
+    int count=0;
+    for (int i=1; num!=0;i++)
+    {
+        num/=10;
+        count=i;
+        
+    }
+    return count;
+}
+
+Console.Write("Input number: ");
+int N=int.Parse(Console.ReadLine()!);
+Console.WriteLine(kolC(N));
