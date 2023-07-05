1ed5c038c2f6695ab968d98877d933b566e81404
AndreiE <andre.nikolasius@gmail.com>
2023-07-05 18:39:09 +0300

    add HomeWork 4_25

---
+++ Lesson_4/HomeWork_4_25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int stepen(int a, int b)
+{
+    int res = 1;
+    if (a == 0) res=0;
+    for(int i = 1; i <= b;i++)
+    {
+        res*=a;
+    }
+    return res;
+}
+
+Console.Write("Input A: ");
+int A=int.Parse(Console.ReadLine()!);
+Console.Write("Input B: ");
+int B=int.Parse(Console.ReadLine()!);
+Console.Write($"A^B = {stepen(A,B)}");
