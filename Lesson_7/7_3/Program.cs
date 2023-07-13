2901604d7e24a4fee478e5fd8f23db8646d0db32
AndreiE <andre.nikolasius@gmail.com>
2023-07-13 21:23:13 +0300

    add class work 7_3 in lesson 7

---
+++ Lesson_7/7_3/Program.cs
@@ -0,0 +1,46 @@
+﻿// 3. Задайте двумерный массив. Найдите сумму элементов главной диагонали.
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            System.Console.Write($"{matr[i, j], 4} ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+int EvenElement(int[,] arr)
+{
+    int sumA=0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i==j) sumA+= arr[i, j];
+        }
+    }
+    return sumA;
+}
+
+Console.Write("Введите количество строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+PrintArray(matrix);
+System.Console.WriteLine($"Сумма главной диагонали равна: {EvenElement(matrix)}");
