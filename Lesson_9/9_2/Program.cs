32df3f83ffc6dbaba0be6281ccf7ff6b61d26fbf
AndreiE <andre.nikolasius@gmail.com>
2023-07-20 21:35:24 +0300

    add class work 9-2 in lesson 9

---
+++ Lesson_9/9_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 3. Напишите программу, которая будет принимать на вход число
+//    и возвращать сумму его цифр. Использовать рекурсию.
+
+int SummaN(int n)
+{
+    int summa=0;
+    if (n/10==0) return n;
+    return SummaN(n/10)+n%10; 
+
+}
+Console.Clear();
+Console.Write("Введите число n: ");
+int N=int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр числа равен {SummaN(N)}");
